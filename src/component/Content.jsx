012c2b9bc8776bc82d04238e446fa88347056049
SHAHINA AKTER <80826931+shahina0199@users.noreply.github.com>
2023-07-13 05:24:12 -0700

    Update Content.jsx

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
+ 
+
+const Content = () => {
+  const [count, setCount] = useState(5);
+  console.log(count);
   
- 
- const Content = () => {
-    return (
-        <div>
-           <form>
-            <label>
-              Name:
-               <input type="text" name="name" />
-               </label>
-              <input type="submit" value="Submit" />
-             </form> 
-        </div>
-    );
- };
- 
- export default Content;
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaseCount= () => {
+    setCount(count - 1);
+  };
+  
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center">Count:{count}</h1>
+      <button onClick={increaseCount} className="bordar:1px, padding:20px 30px">
+        Increase
+      </button>
+      <button onClick={decreaseCount} className="bordar:1px, padding:20px 30px">
+      decrease
+      </button>
+
+      
+    </div>
+  );
+};
+
+export default Content;
