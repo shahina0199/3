7a5bfa097ac4765f687be5e61bef18bb243cdbda
SHAHINA AKTER <80826931+shahina0199@users.noreply.github.com>
2023-07-15 21:00:56 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
  
 import Header from './component/Header';
-
 import Content from './component/Content';
- import Footer from './component/Footer';
+import Footer from './component/Footer';
+ 
 
-const App = () => {
+function App() {
   return (
-    <>
+    <div className="App">
       <Header title="My React App" />
-      <Content/>
-     <Footer title="Shahina Lopa" des="My Country is bast"/>
-       
-    </>
+      <Content />
+      <Footer />
+    </div>
   );
-};
+}
 
 export default App;
-
-
-
-
-
-
-
- 
