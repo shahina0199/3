2fd070473a6023df5debf464f261aa5f28e9f54a
SHAHINA AKTER <80826931+shahina0199@users.noreply.github.com>
2023-07-16 04:56:23 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,43 @@
  
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./App.css"
+ 
+.App {
+    text-align: center;
+    margin: 50px;
+  }
+  
+  
+  
+  textarea {
+    width: 300px;
+     height: 120px;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+  
+  button {
+    margin-bottom: 10px;
+    border: 1px solid rgb(39, 37, 37);
+    padding: 13px 30px;
+    border-radius: 23px ;
+    align-items: center;
+    cursor: pointer;
+    
+  }
+  .button-1{
+    align-items: center;
+  }
+
+  .Count{
+
+    text-align: center;
+  }
+  
+    
+  button {
+    margin-bottom: 10px;
+  }
+  
+
 
  
 
